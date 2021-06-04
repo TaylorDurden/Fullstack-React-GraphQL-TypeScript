7e4f05a081d686a5fb92154ddffc8039ff73737a
TaylorLee <lizhen008@chinasofti.com>
2021-06-04 19:28:20 +0800

    set if user is logged in

--- webapp/src/components/NavBar.tsx
+++ webapp/src/components/NavBar.tsx
@@ -1,15 +1,24 @@
-import { Box, Flex, Link } from '@chakra-ui/react';
+import { Box, Flex, Link, } from '@chakra-ui/react';
 import React from 'react'
 import NextLink from "next/link";
+import { useMeQuery } from '../generated/graphql';
 
 interface NavBarProps {
 
 }
 
 export const NavBar: React.FC<NavBarProps> = ({ }) => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml='auto'>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  // data is fetching
+  if (fetching) {
+    // user not logged in
+  }
+  else if (!data?.me) {
+    // user is not logged in
+    body = (
+      <>
         <NextLink href="/login">
           <Link color="white" mr={2}>Login</Link>
         </NextLink>
@@ -17,6 +26,19 @@ export const NavBar: React.FC<NavBarProps> = ({ }) => {
         <NextLink href="/register">
           <Link color="white">Register</Link>
         </NextLink>
+      </>
+    );
+  }
+  else {
+    body = (
+      <Box>{data.me.username}</Box>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml='auto'>
+        {body}
       </Box>
     </Flex>
   );
